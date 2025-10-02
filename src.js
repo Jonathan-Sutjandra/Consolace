002aa6e8aa7a24965a5f27f2f1e80b120bd0b252
Jonathan Sutjandra <zonzeluc2@gmail.com>
2025-10-02 13:27:20 -0400

    trying to get autofocus on the text input

--- src.js
+++ src.js
@@ -1,1 +1,6 @@
-document.getElementById("inputline").focus();
+let gamestate = "console"; //gamestate will be how the game knows what is going on. right now, it will be the default playing state,
+                           //"console, for coding reasons."
+
+while (gamestate = "console") {
+    document.getElementById("inputline").focus();
+}
