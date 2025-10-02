1139cbaa62fc33bd434a48491e25c2f1d9d05a3e
Jonathan Sutjandra <zonzeluc2@gmail.com>
2025-10-02 14:57:23 -0400

    finally some functionality: textarea that scales with window added

--- src.js
+++ src.js
@@ -1,6 +1,13 @@
-let gamestate = "console"; //gamestate will be how the game knows what is going on. right now, it will be the default playing state,
+let gamestate = "bootup"; //gamestate will be how the game knows what is going on. right now, it will be the default playing state,
                            //"console, for coding reasons."
 
-while (gamestate = "console") {
+document.getElementById("startbutton").addEventListener("click", () => {
+    gamestate = "console";
+    document.getElementById("inputline").style.display = "block";
+    document.getElementById("startbutton").style.display = "none";
+});
+
+
+while (gamestate == "console") {
     document.getElementById("inputline").focus();
 }
